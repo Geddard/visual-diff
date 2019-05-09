b7829de6cce1817f044b13b7a9df5b7c0b79171b
Javier Bures <jbures@gog.com>
2019-05-09 11:26:57 +0200

    Adding react app

--- src/electron.js
+++ src/electron.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow () {
   let window = new BrowserWindow({ width: 1024, height: 768 });
-  window.loadURL("http://localhost:8080");
+  window.loadURL("http://localhost:3000");
 }
 
 app.on("ready", createWindow);
