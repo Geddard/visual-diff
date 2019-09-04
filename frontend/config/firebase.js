5d973cb54bc50ada53c910a01f3e4ec122eb4cac
Javier Bures <jbures@gog.com>
2019-09-04 12:17:26 +0200

    Uploading jpg :)

--- frontend/config/firebase.js
+++ frontend/config/firebase.js
@@ -1,13 +1,18 @@
 const firebase = require('firebase');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 require('firebase/database');
+require('firebase/storage');
+
+// Workaround for firebase not having this installed, beacuse reasons.
+global.XMLHttpRequest = require("xhr2");
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCIVB0g51AzDFcgeOvx7dpQeKKPUqoDbSs',
   authDomain: 'visual-diff.firebaseapp.com',
   databaseURL: 'https://visual-diff.firebaseio.com',
   projectId: 'visual-diff',
-  storageBucket: '',
+  storageBucket: 'gs://visual-diff.appspot.com/',
   messagingSenderId: '586736836008',
   appId: '1:586736836008:web:9f12fe0ae660eabf'
 };
@@ -25,10 +30,23 @@ const save = (config) => {
   });
 };
 
+// TODO: figure out flow for uploading images
+const upload = () => {
+  const storageRef = firebase.storage().ref();
+  const imageRef = storageRef.child('mountains.jpg');
+
+  const jpegData = fs.readFileSync('./public/frontpage.jpg');
+
+  imageRef.put(jpegData).then(function(snapshot) {
+    console.log('Uploaded an array!');
+  }).catch(error => console.log("Error", error));
+}
+
 exports.firebaseApp = firebaseApp;
 exports.setFirebaseEndpoint = (app) => {
   app.post('/api/save', bodyParser.json(), (req, res) => {
     save(req.body);
     res.json("Saved");
   });
+
 };
